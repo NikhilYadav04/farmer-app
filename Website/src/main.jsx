27fe1ffdf168213b388e43c52afbf40e25ef01d2
2017kartik <kartikagarwal9119063420@gmail.com>
2025-02-14 11:22:19 +0530

    Setting up file structure

--- Website/src/main.jsx
+++ Website/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Auth0Provider } from '@auth0/auth0-react'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
